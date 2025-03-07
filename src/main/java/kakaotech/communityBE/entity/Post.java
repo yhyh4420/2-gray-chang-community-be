69a4b3065977046c34aa03a221a0fe5ca6bfe9b6
Yehun <yhyh4420@naver.com>
2025-03-08 01:24:46 +0900

    feat: 엔티티 추가

--- src/main/java/kakaotech/communityBE/entity/Post.java
+++ src/main/java/kakaotech/communityBE/entity/Post.java
@@ -1,4 +1,50 @@
 package kakaotech.communityBE.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@Table(name = "post")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Column
+    private String image;
+
+    @Column
+    private int likes = 0;
+
+    @Column
+    private int views = 0;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime postDate;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+
 }
