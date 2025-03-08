42834882428a43facc1d613431bc3b255155a616
Yehun <yhyh4420@naver.com>
2025-03-08 21:37:33 +0900

    feat: Post 서비스 추가

--- src/main/java/kakaotech/communityBE/controller/PostController.java
+++ src/main/java/kakaotech/communityBE/controller/PostController.java
@@ -1,4 +1,21 @@
 package kakaotech.communityBE.controller;
 
+import kakaotech.communityBE.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+
 }
--- src/main/java/kakaotech/communityBE/service/PostService.java
+++ src/main/java/kakaotech/communityBE/service/PostService.java
@@ -1,4 +1,19 @@
 package kakaotech.communityBE.service;
 
+import kakaotech.communityBE.repository.PostRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PostService {
+
+    private static final Logger logger = LoggerFactory.getLogger(PostService.class);
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 }
