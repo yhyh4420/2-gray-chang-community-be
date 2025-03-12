72e227929af639d482edc21645ea52e4f2af8623
Yehun <yhyh4420@naver.com>
2025-03-12 15:50:45 +0900

    feat: CommentController 추가

---
+++ src/main/java/kakaotech/communityBE/controller/CommentController.java
@@ -0,0 +1,11 @@
+package kakaotech.communityBE.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comment")
+@CrossOrigin(origins = "http://localhost:5500")
+public class CommentController {
+}
