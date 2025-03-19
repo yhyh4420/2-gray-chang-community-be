40e152f39b675924e38691dc5a7b8a1ee8209d69
Yehun <yhyh4420@naver.com>
2025-03-19 14:20:45 +0900

    perf: n+1문제 해결 쿼리 작성

--- src/main/java/kakaotech/communityBE/repository/PostRepository.java
+++ src/main/java/kakaotech/communityBE/repository/PostRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface PostRepository extends JpaRepository<Posts, Long> {
@@ -14,4 +17,10 @@ public interface PostRepository extends JpaRepository<Posts, Long> {
     @Modifying
     @Query("update Posts p set p.likes = p.likes+1 where p.id = :postId")
     int increaseLikes(@Param("postId") Long postId);
+
+    @Query("select p from Posts p left join fetch p.comments")
+    List<Posts> findAllFetch();
+
+    @Query("select p from Posts p left join fetch p.comments where p.id = :postId")
+    Optional<Posts> findbyIdFetch(@Param("postId") Long postId);
 }
