daec55ad65ca015ba50b0c081ede2a7936299257
Yehun <yhyh4420@naver.com>
2025-03-21 07:59:37 +0900

    test: 쿼리 개수 확인하여 주석에 작성

--- src/test/java/kakaotech/communityBE/repository/PostRepositoryQueryTest.java
+++ src/test/java/kakaotech/communityBE/repository/PostRepositoryQueryTest.java
@@ -33,10 +33,10 @@ class PostRepositoryQueryTest {
 
     @BeforeEach
     void setUp() {
-        postId = postRepository.findAll().get(random.nextInt(1000)).getId();
+        postId = postRepository.findAll().get(random.nextInt(1)).getId();
     }
 
-
+    // todo : 쿼리 발행 개수 세기
     @Test
     void findAll_성능비교() {
         //n+1문제 발생
@@ -73,20 +73,23 @@ class PostRepositoryQueryTest {
     }
 
     /*
-    ----------------------------------------
-    Seconds       %       Task name
-    ----------------------------------------
-    0.037745709   74%     일반 findAll
-    0.010377709   20%     entityGraph findAll
-    0.002919583   06%     fetch join findAll
+    ----------------------------------------------------------------
+    Seconds       %       발행 쿼리 개수       Task name
+    ----------------------------------------------------------------
+    0.037745709   74%     1+5              일반 findAll
+    0.010377709   20%     1                entityGraph findAll
+    0.002919583   06%     1                fetch join findAll
 
-    일반 findAll보다 fetch join의 성능이 92% 향상되었다.
+    fetch join으로 post별 comment를 찾는 쿼리 수정 결과
+    1. 쿼리 발행 숫자가 5+1에서 1개로 감소했다(N+1문제 해결)
+    2. 일반 findAll보다 fetch join의 속도가 1,133% 향상되었다.
      */
 
     @Test
     void findById_성능비교(){
         System.out.println("----------------------");
         // n+1 문제 발생
+        em.clear();
         stopWatch.start("일반 findById");
         Posts post = postRepository.findById(postId).isPresent() ? postRepository.findById(postId).get() : null;
         for (Comment comment : post.getComments()) {
@@ -94,6 +97,7 @@ class PostRepositoryQueryTest {
         }
         System.out.println();
         stopWatch.stop();
+        em.clear();
         System.out.println("----------------------");
         stopWatch.start("fetch join findById");
         Posts postfetch = postRepository.findbyIdFetch(postId).isPresent() ? postRepository.findbyIdFetch(postId).get() : null;
@@ -106,11 +110,11 @@ class PostRepositoryQueryTest {
     }
 
     /*
-    ----------------------------------------
-    Seconds       %       Task name
-    ----------------------------------------
-    0.005303667   44%     일반 findById
-    0.006709583   56%     fetch join findById
+    ----------------------------------------------------------------
+    Seconds       %       발행 쿼리 개수       Task name
+    ----------------------------------------------------------------
+    0.005303667   44%     2             일반 findById
+    0.006709583   56%     2             fetch join findById
 
     결과를 보면 일반 findbyId가 더 성능이 좋다. 생각해보면 당연한게, findbyId는 원래 한개의 엔티티만 조회한다.
     오히려 괜한 inner join 연산으로 성능이 떨어진 것이다.
