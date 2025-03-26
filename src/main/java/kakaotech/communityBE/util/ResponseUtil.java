c1ae8e80f9569ce3aee31789c704c53a12c3700b
Yehun <yhyh4420@naver.com>
2025-03-26 17:36:07 +0900

    refactor: 미사용 메서드 삭제

--- src/main/java/kakaotech/communityBE/util/ResponseUtil.java
+++ src/main/java/kakaotech/communityBE/util/ResponseUtil.java
@@ -14,23 +14,11 @@ public class ResponseUtil {
         return ResponseEntity.ok(new MessageResponseDto(message));
     }
 
-    public static <T> ResponseEntity<T> created(T entity) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(entity);
-    }
-
     public static ResponseEntity<MessageResponseDto> created(String message) {
         return ResponseEntity.status(HttpStatus.CREATED).body(new MessageResponseDto(message));
     }
 
-    public static <T> ResponseEntity<T> noContent(T entity) {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(entity);
-    }
-
     public static ResponseEntity<MessageResponseDto> noContent(String message) {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new MessageResponseDto(message));
-    }
-
-    public static <T> ResponseEntity<T> withStatus(HttpStatus status, T body) {
-        return ResponseEntity.status(status).body(body);
     }
 }
